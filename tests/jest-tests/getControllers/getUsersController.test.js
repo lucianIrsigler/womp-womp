8b258f8e1c283caa8cd5c2ad765b936e752a416e
lucianIrsigler <l.irsigler64@gmail.com>
2024-05-18 19:58:51 +0200

    Added

--- tests/jest-tests/getControllers/getUsersController.test.js
+++ tests/jest-tests/getControllers/getUsersController.test.js
@@ -14,6 +14,9 @@ jest.mock("../../../models", () => ({
   },
   User: {
     find: jest.fn(),
+  },
+  Applicant: {
+    find: jest.fn(),
   }
 }));
 
@@ -171,4 +174,83 @@ describe("Testing getAdmins", () => {
     expect(res.json).toHaveBeenCalledWith({"error": "Assignment to constant variable."});
   });
 
+});
+
+
+
+
+describe("Testing getApplicants controller", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return all applicants if no query parameters are provided", async () => {
+    const req = {
+      query: {},
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const mockApplicants = [
+      { id: 1, name: "Applicant 1" },
+      { id: 2, name: "Applicant 2" },
+    ];
+    Applicant.find.mockResolvedValue(mockApplicants);
+
+    await getApplicants(req, res);
+
+    expect(Applicant.find).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(mockApplicants);
+  });
+
+  it("should filter applicants based on query parameters", async () => {
+    const req = {
+      query: {
+        name: "Applicant 1",
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const mockApplicants = [
+      { id: 1, name: "Applicant 1" },
+      { id: 2, name: "Applicant 2" },
+    ];
+    Applicant.find.mockResolvedValue(mockApplicants);
+
+    await getApplicants(req, res);
+
+    expect(Applicant.find).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith([{ id: 1, name: "Applicant 1" }]);
+  });
+
+  it("should filter applicants based on nested query parameters", async () => {
+    const req = {
+      query: {
+        "nested.property": "value",
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const mockApplicants = [
+      { id: 1, nested: { property: "value" } },
+      { id: 2, nested: { property: "otherValue" } },
+    ];
+    Applicant.find.mockResolvedValue(mockApplicants);
+
+    await getApplicants(req, res);
+
+    expect(Applicant.find).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith([{ id: 1, nested: { property: "value" } }]);
+  });
 });
