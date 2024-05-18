2f30c94fe329797e540ffe3fd23930a377474887
lucianIrsigler <l.irsigler64@gmail.com>
2024-05-18 19:56:05 +0200

    Added

--- tests/jest-tests/getControllers/getUsersController.test.js
+++ tests/jest-tests/getControllers/getUsersController.test.js
@@ -5,11 +5,16 @@ jest.mock("../../../models/Applicant");
 
 const { FundingManager } = require("../../../models");
 const { getFundingManagers } = require("../../../controllers/getControllers/getUsersController");
+const { User } = require("../../../models");
+const { getAdmins } = require("../../../controllers/getControllers/getUsersController");
 
 jest.mock("../../../models", () => ({
   FundingManager: {
     find: jest.fn(),
   },
+  User: {
+    find: jest.fn(),
+  }
 }));
 
 describe("Testing getFundingManagers controller", () => {
@@ -86,4 +91,84 @@ describe("Testing getFundingManagers controller", () => {
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith([{ id: 1, nested: { property: "value" } }]);
   });
+});
+
+
+
+
+describe("Testing getAdmins", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  
+  it("should return all admins if no query parameters are provided", async () => {
+    const req = {
+      query: {},
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const mockAdmins = [
+      { id: 1, name: "Admin 1", role: "PLATFORM_ADMIN" },
+      { id: 2, name: "Admin 2", role: "PLATFORM_ADMIN" },
+    ];
+    User.find.mockResolvedValue(mockAdmins);
+
+    await getAdmins(req, res);
+
+    expect(User.find).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(mockAdmins);
+  });
+
+  it("Test for Assignment to constant variable", async () => {
+    const req = {
+      query: {
+        name: "Admin 1",
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const mockAdmins = [
+      { id: 1, name: "Admin 1", role: "PLATFORM_ADMIN" },
+      { id: 2, name: "Admin 2", role: "PLATFORM_ADMIN" },
+    ];
+    User.find.mockResolvedValue(mockAdmins);
+
+    await getAdmins(req, res);
+
+    expect(User.find).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({"error": "Assignment to constant variable."});
+  });
+
+  it("should filter admins based on nested query parameters", async () => {
+    const req = {
+      query: {
+        "nested.property": "value",
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const mockAdmins = [
+      { id: 1, nested: { property: "value" }, role: "PLATFORM_ADMIN" },
+      { id: 2, nested: { property: "otherValue" }, role: "PLATFORM_ADMIN" },
+    ];
+    User.find.mockResolvedValue(mockAdmins);
+
+    await getAdmins(req, res);
+
+    expect(User.find).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({"error": "Assignment to constant variable."});
+  });
+
 });
